9de596844081e520d19e8211bfecf769016e07de
MothusiMasibi <mothusi.masibi@gmail.com>
2016-08-28 13:12:01 +0200

    Login produces error message when user is incorrect.

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -11,9 +11,11 @@
 angular.module('powerCloud')
     .controller('LoginCtrl', function($scope, $location) {
         $scope.submit = function() {
+        $scope.errorVal = false;
 
             var email = $scope.user.email;
             var pass = $scope.user.password;
+
             firebase.auth().signInWithEmailAndPassword(email, pass)
                 .then(function(result)
                 {
@@ -22,9 +24,12 @@ angular.module('powerCloud')
                 })
                 .catch(function(error)
                 {
-                var errorCode = error.code;
-                var errorMessage = error.message;
-                    alert(errorCode + " " + errorMessage);
+                    $scope.errorVal = true;
+                    $scope.errorCode = error.code;
+                    $scope.errorMessage = error.message;
+
+                    console.log(error.message);
+                    $scope.$apply();
                 });
 
             return false;
