6eb989cf9cd4e375752a72e28ed63143ddbdea72
stuartandrews93 <u12153983@tuks.co.za>
2016-10-05 11:58:49 +0200

    AddDevice branch

--- app/scripts/controllers/devices.js
+++ app/scripts/controllers/devices.js
@@ -13,5 +13,20 @@ angular.module('powerCloud')
     .controller('DevicesCtrl', function($scope,  $state) {
         $scope.$state = $state;
 
+        $scope.meta = {};
 
+        $scope.addDevice = function(device)
+        {
+            console.log(device);
+            $scope.meta = angular.copy(device);
+
+            // Get a key for a new Post.
+            var newPostKey = $scope.meta.id;
+
+            // Write the new post's data simultaneously in the posts list and the user's post list.
+            var updates = {};
+            updates['/meta_data/' + newPostKey] = $scope.meta;
+
+            return firebase.database().ref().update(updates);
+        }
     });
